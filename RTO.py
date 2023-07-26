a69ef9da98fa4e5e92f4b4aa82044af744e4573d
harikrishnan998 <122348292+harikrishnan998@users.noreply.github.com>
2023-07-26 16:37:01 +0530

    Delete RTO.py

--- RTO.py
+++
@@ -1,8 +0,0 @@
-import pyrebase.pyrebase
-
-def rto(no,database):
-    # database.child("RTO").child("KL12F9966").child("num").set("1")
-    db=database.child("RTO").get()
-    if no in db.val():
-        n=database.child("RTO").child(no).child("num").get()
-        return n.val()
